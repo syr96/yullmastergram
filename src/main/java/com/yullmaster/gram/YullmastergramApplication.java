65a4677c924dc82148e2fab38578954ca34215a8
DESKTOP-6KBPV3D\user <syr96@naver.com>
2022-01-26 19:13:26 +0900

    - yullmastergram 최초등록

---
+++ src/main/java/com/yullmaster/gram/YullmastergramApplication.java
@@ -0,0 +1,13 @@
+package com.yullmaster.gram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YullmastergramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YullmastergramApplication.class, args);
+	}
+
+}
